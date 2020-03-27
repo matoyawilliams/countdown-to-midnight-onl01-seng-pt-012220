bdfd593bd876b85a028b6f9d1b171daa2a9a19bf
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-27 03:23:51 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,3 +1,23 @@
-number_of_seconds = 0 
-while number_of_seconds -= 0
-puts "Happy New Year #{number_of_seconds}"
+def countdown(num)
+
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  num -= 1
+
+end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(num)
+
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+
+end
+  "HAPPY NEW YEAR!"
+end
